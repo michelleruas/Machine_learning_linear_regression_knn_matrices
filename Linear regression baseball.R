36a4c5608486d955a1b4594fd2eb200199a80286
Michelle Ruas <michelle.ruas@gmail.com>
2020-04-27 21:13:48 +0100

    Baseball plots

--- Linear regression baseball.R
+++ Linear regression baseball.R
@@ -8,3 +8,13 @@ Teams %>% filter(yearID %in% 1961:2001) %>%
   mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
   ggplot(aes(HR_per_game, R_per_game)) + 
   geom_point(alpha = 0.5)
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(SB_per_game = SB / G, R_per_game = R / G) %>%
+  ggplot(aes(SB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(X3B_per_game = X3B / G, X2B_per_game = R / G) %>%
+  ggplot(aes(X3B_per_game, X2B_per_game)) + 
+  geom_point(alpha = 0.5)
